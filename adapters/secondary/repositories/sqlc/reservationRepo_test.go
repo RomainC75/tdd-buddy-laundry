2644e73031e1d3ac62f473a870ecd0212657a180
Romain CHENARD <rom.chenard@gmail.com>
2025-07-19 15:41:53 +0200

    test: 💍 modifying tests

--- adapters/secondary/repositories/sqlc/reservationRepo_test.go
+++ adapters/secondary/repositories/sqlc/reservationRepo_test.go
@@ -1,31 +1,38 @@
-//go:build integration
-// +build integration
-
 package repositories
 
 import (
 	"context"
 	"fmt"
-	db "laundry/adapters/secondary/repositories/sqlc/db/sqlc"
-	"laundry/config"
-	"laundry/core/business-logic/models"
+	"log"
 	"path/filepath"
 	"testing"
 	"time"
 
+	db "laundry/adapters/secondary/repositories/sqlc/db/sqlc"
+	"laundry/config"
+	"laundry/core/business-logic/models"
+
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 	"github.com/testcontainers/testcontainers-go"
 	"github.com/testcontainers/testcontainers-go/modules/postgres"
 	"github.com/testcontainers/testcontainers-go/wait"
 )
 
-func TestCustomerRepository(t *testing.T) {
-	ctx := context.Background()
+type RepoTestSuite struct {
+	suite.Suite
+	pgContainer *postgres.PostgresContainer
+	repo        *ReservationRepository
+	ctx         context.Context
+}
+
+func (suite *RepoTestSuite) SetupTest() {
+	suite.ctx = context.Background()
 	config.Set("postgres", "postgres", "localhost", 5432, "testdb")
 	cfg := config.Get()
 
-	pgContainer, err := postgres.RunContainer(ctx,
+	pgContainer, err := postgres.RunContainer(suite.ctx,
 		testcontainers.WithImage("postgres:15.3-alpine"),
 		postgres.WithInitScripts(filepath.Join("db", "migration", "init_test.sh")),
 		postgres.WithDatabase(cfg.Db.Name),
@@ -37,18 +44,21 @@ func TestCustomerRepository(t *testing.T) {
 	)
 	if err != nil {
 		fmt.Println("-----------> ", err.Error())
-		t.Fatal(err)
+		log.Fatal(err)
 	}
+	suite.pgContainer = pgContainer
 
-	t.Cleanup(func() {
-		if err := pgContainer.Terminate(ctx); err != nil {
-			t.Fatalf("failed to terminate pgContainer: %s", err)
-		}
-	})
-
-	connStr, err := pgContainer.ConnectionString(ctx, "sslmode=disable")
-	assert.NoError(t, err)
+	connStr, err := suite.pgContainer.ConnectionString(suite.ctx, "sslmode=disable")
+	assert.NoError(suite.T(), err)
 	db.ConnectWithString(connStr)
+}
+
+func (suite *RepoTestSuite) TestCustomerRepository(t *testing.T) {
+	// t.Cleanup(func() {
+	// 	if err := suite.pgContainer.Terminate(ctx); err != nil {
+	// 		t.Fatalf("failed to terminate pgContainer: %s", err)
+	// 	}
+	// })
 
 	// customerRepo, err := NewRepository(ctx, connStr)
 	// assert.NoError(t, err)
@@ -61,7 +71,7 @@ func TestCustomerRepository(t *testing.T) {
 	pin := "abc1"
 	machineNum := "a10"
 
-	err = reservationRepo.CreateReservation(ctx, models.Reservation{
+	err := reservationRepo.CreateReservation(suite.ctx, models.Reservation{
 		Id:              newUuid,
 		ReservationDate: now,
 		ReservationTime: reservationTime,
@@ -74,7 +84,7 @@ func TestCustomerRepository(t *testing.T) {
 	// assert.NotNil(t, c)
 
 	store := db.GetConnection()
-	reservations, err := (*store).ListReservations(ctx)
+	reservations, err := (*store).ListReservations(suite.ctx)
 
 	assert.NoError(t, err)
 	assert.Equal(t, len(reservations), 1)
